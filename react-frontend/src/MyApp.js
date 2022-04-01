7e279d2336305c3ad8f344530fd935530e71d968
jordantmiller <43153711+jordantmiller@users.noreply.github.com>
2022-04-01 13:40:23 -0700

    Add delete button

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Table from './Table'
 
-const characters = [
+
+function MyApp() {
+  const [characters, setCharacters] = useState([
     {
       name: 'Charlie',
       job: 'Janitor',
@@ -18,12 +20,18 @@ const characters = [
       name: 'Dennis',
       job: 'Bartender',
     },
-];
+  ]);
 
-function MyApp() {
+  function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index
+    });
+    setCharacters(updated);
+  }
+
     return (
         <div className="container">
-          <Table characterData={characters} />
+          <Table characterData={characters} removeCharacter={removeOneCharacter} />
         </div>
     )
 }
